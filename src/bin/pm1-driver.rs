befeeff160e40841e67d71b2e8cec773b6109652
YdrMaster <ydrml@hotmail.com>
2021-10-11 13:38:34 +0800

    feat: 适配驱动层更新

--- src/bin/pm1-driver.rs
+++ src/bin/pm1-driver.rs
@@ -1,18 +1,17 @@
 ﻿use driver::{Driver, SupersivorEventForSingle::*, SupervisorForSingle};
-use pm1_sdk::PM1Supervisor;
-use std::{thread, time::Duration};
+use std::{thread::sleep, time::Duration};
 
 fn main() {
-    PM1Supervisor::new().join(|e| {
+    SupervisorForSingle::<String, pm1_sdk::PM1>::new().join(|e| {
         match e {
             Connected(_, driver) => println!("Connected: {}", driver.status()),
             ConnectFailed => {
                 println!("Failed.");
-                thread::sleep(Duration::from_secs(1));
+                sleep(Duration::from_secs(1));
             }
             Disconnected => {
                 println!("Disconnected.");
-                thread::sleep(Duration::from_secs(1));
+                sleep(Duration::from_secs(1));
             }
             Event(_, Some((_, event))) => println!("Event: {:?}", event),
             Event(_, None) => {}
